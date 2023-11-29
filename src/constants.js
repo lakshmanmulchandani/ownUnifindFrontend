e9ee6cba68e458c23e6de84909457b62b3ef72a7
lakshmanmulchandani <mulchandanilakshman@gmail.com>
2023-11-29 11:25:43 +0530

    link added

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 export const BACKEND_URL = "https://unifind-backend.onrender.com" || "http://localhost:5000"
-export const FRONTEND_URL = process.env.REACT_FRONTEND_URL || "http://localhost:3000"
+export const FRONTEND_URL = "unifind.netlify.app" || "http://localhost:3000"
