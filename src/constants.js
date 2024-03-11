8e4d5fca0211288a64a7f6681ce2f5363f811198
lakshmanmulchandani <mulchandanilakshman@gmail.com>
2024-03-11 14:50:37 +0530

    link updated

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "https://unifind-backend.onrender.com" || "http://localhost:5000"
-export const FRONTEND_URL = "unifind.netlify.app" || "http://localhost:3000"
+export const BACKEND_URL = "https://ownunifindbackend.onrender.com" || "http://localhost:5000"
+export const FRONTEND_URL = "https://unifindnitrr.netlify.app" || "http://localhost:3000"
